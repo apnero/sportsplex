1d155030844dd48a500eda12068a7be5a63f1896
Andrew Nero <andrew.nero@gmail.com>
2015-04-29 21:25:36 -0400

    web.js fix

--- web.js
+++ web.js
@@ -1,40 +1,27 @@
 var express = require('express');
-var pg = require('pg');
-
 var app = express.createServer();
 app.use(express.static(__dirname ));
 
 app.use(express.bodyParser());
 
-var postmark = require("postmark")("f403df90-28cf-4da4-9819-eaa0773a7b83")
+var postmark = require("postmark")("ff4a1e02-a2bf-4b6a-9a1d-763471e06995")
+
 
 
 app.post('/getFormData', function(req, res) {
   postmark.send({
     "From": "email@plasmascape.com",
-    "To": "andrew@plasmascape.com",
-    "Subject": "EMAIL FROM PHILLYS WEB SITE",
-    "TextBody": new String('From: ' + req.body.name + '.\n\rEmail: ' + req.body.email  + '.\n\rSubject: ' + req.body.phone + '.\n\rMessage: ' + req.body.message)
+    "To": "fighttactix@gmail.com",
+    "Subject": "EMAIL FROM FightTactiX WEBSITE",
+    "TextBody": new String('From: ' + req.body.name + '.\n\rPhone: ' + req.body.phone + '.\n\rEmail: ' + req.body.email + '.\n\rSubject: ' + req.body.subject + '.\n\rMessage: ' + req.body.message)
 	}, function(error, success) {
     if(error) {
-        res.send("We have had technical difficulties and we regret to say you email hasn't been delivered to PlasmaGFX");
+        res.send("We have had technical difficulties and we regret to say you email hasn't been delivered to Cannon Ridge");
        return;
     }
     res.send("Thank you for contacting us.  We will respond as soon as possible.")
 	});
   
-});
-
-app.post('/getEmailData', function(req, res) {
-	
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	  client.query("INSERT INTO emails VALUES ($1)", [req.body.email], function(err, result) {
-		done();
-		if(err) return console.error(err + 'in here');
-		console.log(result.rows);
-	  });
-	});
-
 });
 
 
